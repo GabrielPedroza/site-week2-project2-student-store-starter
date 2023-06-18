6dcde288657f36e7f145b174261530446aba0ca8
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-06-18 13:19:53 -0700

    basic sidebar structure

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,37 @@
-import * as React from "react"
+import React, { useState } from "react"
+import { MdOutlineAddShoppingCart } from "react-icons/md"
+import { AiOutlineArrowRight } from "react-icons/ai"
 import "./Sidebar.css"
 
 export default function Sidebar() {
-  return (
-    <section className="sidebar">
-      <p>Sidebar</p>
-    </section>
-  )
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleToggle = () => {
+		setIsOpen(!isOpen)
+	}
+
+	return (
+		<aside
+			className={`sidebar ${isOpen ? "open" : ""}`}
+			style={{ width: 500, backgroundColor: "#2c3f47" }} // setting width in css file did not work
+		>
+			<div className="toggle-btn" onClick={handleToggle}>
+				{isOpen ? <p>Close</p> : <AiOutlineArrowRight />}
+			</div>
+			<div>
+				<div>
+					<MdOutlineAddShoppingCart />
+				</div>
+			</div>
+			<div className="sidebar-content">
+				<h2>Menu</h2>
+				<ul className="menu-list">
+					<li>Home</li>
+					<li>Products</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</aside>
+	)
 }
