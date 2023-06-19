1801a626434c6ac6f6d6f2a6b2ee9b9bc04b2709
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-06-19 13:54:36 -0700

    Navbar component complete with appropriate media queries set in place

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,36 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa"
+import Logo from "../Logo/Logo"
 import "./Navbar.css"
 
 export default function Navbar() {
 	return (
-		<nav className="navbar">
-			<Link to="/" className="navbar-brand">
-				College of CodePath
-			</Link>
-			<div className="navbar-links">
-				<Link to="/" className="navbar-link">
+		<nav className="nav-container">
+			<Logo />
+			<div className="nav-socials">
+				<a href="https://twitter.com" className="nav-social-link">
+					<FaTwitter />
+				</a>
+				<a href="https://instagram.com" className="nav-social-link">
+					<FaInstagram />
+				</a>
+				<a href="https://facebook.com" className="nav-social-link">
+					<FaFacebook />
+				</a>
+			</div>
+			<div className="nav-links">
+				<Link to="/" className="nav-link">
 					Home
 				</Link>
-				<Link to="/about" className="navbar-link">
+				<Link to="/about" className="nav-link">
 					About
 				</Link>
-				<Link to="/contact" className="navbar-link">
+				<Link to="/contact" className="nav-link">
 					Contact
+				</Link>
+				<Link to="/contact" className="nav-link">
+					Buy now
 				</Link>
 			</div>
 		</nav>
