d7eb9d9e42ab626b5c4b2d9ef4de39c34c40e92c
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-06-19 17:54:13 -0700

    loading spinner | displaying products data from api

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,31 @@
-import * as React from "react"
+import React from "react"
+import { AiOutlineLoading } from "react-icons/ai"
+import Hero from "../Hero/Hero"
 import "./Home.css"
 
-export default function Home() {
-  return (
-    <div className="home">
-      <p>Home</p>
-    </div>
-  )
+export default function Home({ products }) {
+	console.log(products)
+
+	return (
+		<div className="home">
+			<Hero />
+			<p>home</p>
+			{products.products ? (
+				products.products.map(product => {
+					return (
+						<div key={product.id}>
+							<p>{product.name}</p>
+							<p>{product.description}</p>
+							<p>{product.price}</p>
+							<p>{product.image}</p>
+						</div>
+					)
+				})
+			) : (
+				<div className="loading-spinner">
+					<AiOutlineLoading className="spinner" />
+				</div>
+			)}
+		</div>
+	)
 }
