5cab839fee74e937903acf9a29804d0807b93cd5
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-20 16:12:08 -0700

    removed logic to place it in its own component (ProductGrid)

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,48 +1,20 @@
 import React from "react"
-import { AiOutlineLoading } from "react-icons/ai"
-import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi"
 import Hero from "../Hero/Hero"
 import "./Home.css"
 import About from "../About/About"
 import Contact from "../Contact/Contact"
+import Footer from "../Footer/Footer"
+import ProductGrid from "../ProductGrid/ProductGrid"
 
 export default function Home({ products }) {
 	return (
 		<div className="home">
 			<Hero />
 			<p>search bar goes here</p>
-			<h3>Best Selling Products</h3>
-			<div className="product-grid">
-				{products.products ? (
-					products.products.map(product => (
-						<div key={product.id} className="product-item">
-							<img src={product.image} alt={product.name} />
-							<div className="product-name-count">
-								<p>{product.name}</p>
-								<div>
-									<HiOutlinePlus className="sign plus"/>
-									<HiOutlineMinus className="sign minus" />
-								</div>
-							</div>
-								<p className="product-price">{formatPrice(product.price)}</p>
-								<div className="stars">
-									<img src="/stars.png" alt="stars" />
-								</div>
-						</div>
-					))
-				) : (
-					<div className="loading-spinner">
-						<AiOutlineLoading className="spinner" />
-					</div>
-				)}
-			</div>
+			<ProductGrid products={products}/>
 			<About />
 			<Contact />
+			<Footer />
 		</div>
 	)
 }
-
-function formatPrice(price) {
-	const formattedPrice = price % 1 === 0 ? price.toFixed(0) : price.toFixed(2);
-	return `$${formattedPrice}`;
-  }
