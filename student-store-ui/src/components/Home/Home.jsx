e6752ca16ccf7e4d9791b6ca3ca3053eaed43216
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-06-20 08:37:44 -0700

    home component with data and basic styling

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -2,30 +2,31 @@ import React from "react"
 import { AiOutlineLoading } from "react-icons/ai"
 import Hero from "../Hero/Hero"
 import "./Home.css"
+import About from "../About/About"
+import Contact from "../Contact/Contact"
 
 export default function Home({ products }) {
-	console.log(products)
-
 	return (
 		<div className="home">
 			<Hero />
 			<p>home</p>
-			{products.products ? (
-				products.products.map(product => {
-					return (
-						<div key={product.id}>
+			<div className="product-grid">
+				{products.products ? (
+					products.products.map(product => (
+						<div key={product.id} className="product-item">
+							<img src={product.image} alt={product.name} />
 							<p>{product.name}</p>
-							<p>{product.description}</p>
 							<p>{product.price}</p>
-							<p>{product.image}</p>
 						</div>
-					)
-				})
-			) : (
-				<div className="loading-spinner">
-					<AiOutlineLoading className="spinner" />
-				</div>
-			)}
+					))
+				) : (
+					<div className="loading-spinner">
+						<AiOutlineLoading className="spinner" />
+					</div>
+				)}
+			</div>
+			<About />
+			<Contact />
 		</div>
 	)
 }
