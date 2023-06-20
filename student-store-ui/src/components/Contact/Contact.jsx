7b2455dcc4be946e35222cc1d1abce38a007f31c
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-20 11:01:54 -0700

    Contact component

---
+++ student-store-ui/src/components/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    <>
+    <p>Contact Us</p>
+    <div>
+      <p>Email:
+code@path.org
+Phone:
+1-800-CODEPATH
+Address:
+123 Fake Street, San Francisco, CA
+Socials:</p>
+      <img src="/Contact-Us-Person.png" alt="happy person" />
+    </div>
+    </>
+  )
+}
+
+export default Contact
