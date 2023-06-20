919e77b184e7505898ed45462b5f73d71227e3e4
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-20 11:02:37 -0700

    Home component with better styling and react icons

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { AiOutlineLoading } from "react-icons/ai"
+import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi"
 import Hero from "../Hero/Hero"
 import "./Home.css"
 import About from "../About/About"
@@ -15,7 +16,13 @@ export default function Home({ products }) {
 					products.products.map(product => (
 						<div key={product.id} className="product-item">
 							<img src={product.image} alt={product.name} />
-							<p>{product.name}</p>
+							<div className="product-name-count">
+								<p>{product.name}</p>
+								<div>
+									<HiOutlinePlus className="sign plus"/>
+									<HiOutlineMinus className="sign minus" />
+								</div>
+							</div>
 							<p>{product.price}</p>
 						</div>
 					))
