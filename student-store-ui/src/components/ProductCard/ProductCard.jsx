3bc999ae113e31b22972ff4f6b79a1d5cc1884fe
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-21 09:59:10 -0700

    Implement ProductCard and encapsulated logic in its own component

---
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,28 @@
+import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi"
+import "./ProductCard.css"
+
+const ProductCard = ({ product }) => {
+  return (
+    <div key={product.id} className="product-item">
+        <img src={product.image} alt={product.name} />
+        <div className="product-name-count">
+            <p>{product.name}</p>
+            <div>
+                <HiOutlinePlus className="sign plus"/>
+                <HiOutlineMinus className="sign minus" />
+            </div>
+        </div>
+            <p className="product-price">{formatPrice(product.price)}</p>
+            <div className="stars">
+                <img src="/stars.png" alt="stars" />
+            </div>
+    </div>
+  )
+}
+
+function formatPrice(price) {
+	const formattedPrice = price % 1 === 0 ? price.toFixed(0) : price.toFixed(2);
+	return `$${formattedPrice}`;
+}
+
+export default ProductCard
